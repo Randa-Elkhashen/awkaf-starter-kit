4175833924ee2cb1d7e0fbc794e3f19e842f0b44
kamalMohamed <kmohamed@turndigital.net>
2021-12-22 17:06:06 +0200

    init feature

--- src/modules/global/views/TD-InfintedScroll-Demo.vue
+++ src/modules/global/views/TD-InfintedScroll-Demo.vue
@@ -1,60 +1,28 @@
 <template>
-     <div class="items-wrapper">
-        <div class="item"  v-for="(item , index) in list" :key="index" >
-            <div class="alert alert-success" role="alert">
-                <h4 class="alert-heading">Well done!</h4>
-                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-                <hr>
-                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-            </div>
-        </div>
-    </div>
-
-    <!-- loader -->
-    <scroll-loader :isLoadMore="isMoreData" :onScrollToEnd="getListByStarterKitService" >
-        <div class="my-ldr">
-            <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    </scroll-loader>
-    <!-- loader -->
+    <Listing loader  >
+      <template v-slot="{item}" >
+        <list-item :itemDataModel="item" />
+      </template>
+        <template v-slot:loader>
+            <!-- <img   src="./assets/spinner.gif"/> -->
+            <lazy-card />
+        </template>
+        <template v-slot:emptyMessage>
+              <div class="alert alert-warning">
+                    No Data Found
+              </div>  
+        </template>
+    </Listing>
 </template>
+
 <script>
-    // init bootstrap as global plugin to use in Component styling
-    import axios from "axios"
-    import ScrollLoader from "@/modules/global/components/TD_infiniteScroll/TD_InfiniteScroll.vue"
-    import {  users } from "@/services"
-    export default {
-        name : "listing" ,
-        components : {
-            ScrollLoader
-        } ,
-        data (){
-            return {
-                list : [] , 
-                isMoreData : true ,
-            }
-        } ,
-        methods : {
-            async getList(){
-               let res = await axios.get('https://jsonplaceholder.typicode.com/users')
-                    // handle success
-                     this.list = [ ...this.list , ...res.data]
-                    this.checkIsThereIsMoreData()
-            } ,
-            async getListByStarterKitService(){
-              let res =  await users.getAllUsers()
-                 this.list = [ ...this.list , ...res.data]
-                this.checkIsThereIsMoreData()
-            } ,
-            checkIsThereIsMoreData(){
-                let isNoMoreData = true;
-                if(!isNoMoreData){
-                    debugger
-                    this.isMoreData = false;
-                }
-            }
-        }
-    }
+import Listing from "@/modules/global/components/TD_listing/TD_Listing.vue"
+import ListItem from "@/modules/global/components/TD_genericCard/TD_GenericCard.vue" 
+export default {
+  name: 'App',
+  components : {
+    Listing ,
+    ListItem ,
+  }
+}
 </script>
