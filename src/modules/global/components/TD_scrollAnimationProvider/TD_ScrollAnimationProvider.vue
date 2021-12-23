2f2c003cb9a487c2087b0646b356e4d5418f84b8
kamalMohamed <kmohamed@turndigital.net>
2021-12-23 14:52:44 +0200

    init feature

---
+++ src/modules/global/components/TD_scrollAnimationProvider/TD_ScrollAnimationProvider.vue
@@ -0,0 +1,45 @@
+
+<template>
+            <slot ></slot>
+</template>
+<script>
+const configModel = {
+     // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+}
+import aos from "aos";
+    export default {
+        name: 'ScrollAnimationProvicer',
+        props : {
+            config: {
+                type : Object
+            }
+        } ,
+        computed : {
+            configModel(){
+                return  Object.assign(configModel , this.config)
+            }
+        },
+        mounted(){
+            aos.init(this.configModel)
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    @import "~aos/dist/aos.css";
+</style>
