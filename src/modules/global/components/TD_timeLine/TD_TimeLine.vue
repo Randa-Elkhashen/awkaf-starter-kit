07d84c4aff379096604a0cf64bb6568568454e18
kamalMohamed <kmohamed@turndigital.net>
2021-12-27 12:59:08 +0200

    progress commit

--- src/modules/global/components/TD_timeLine/TD_TimeLine.vue
+++ src/modules/global/components/TD_timeLine/TD_TimeLine.vue
@@ -1,66 +1,34 @@
 
 <template>
-<section class="time-line container-fluid" v-for="( card , index ) in list" :key="index">
-    <animation-provider  :animation="{'data-aos' : 'fade-down' } ">
-        <div class="time-line__row">
-            <h3 class="time-line__row__title">2020</h3>
-            <div  class="time-line__row__card row"
+<section class="td-generic-timeline container-fluid" v-for="( card , index ) in list" :key="index">
+        <div class="td-generic-timeline__row">
+            <h3 class="td-generic-timeline__row__title">2020</h3>
+            <div  class="td-generic-timeline__row__card row"
             :class="[ index%2  ? 'justify-content-end' : 'justify-content-start']"
             >
                 <div class="col-md-6 col-12"   >
-                    <animation-provider :animation="index%2 ? {'data-aos' : 'fade-left' } : {'data-aos' : 'fade-right' }" >
-                        <time-line-card />
-                    </animation-provider>
+                            <slot></slot>
                 </div>
             </div>
         </div>
-    </animation-provider>
 </section>
 </template>
 
 <script>
-import AnimationProvider from '../TD_animationProvider/TD_AnimationProvider.vue';
-import TimeLineCard from '../TD_timeLineCard/TD_TimeLineCard.vue';
+import "./TD_timeline.scss"
+import aos from "aos";
     export default {
-  components: { 
-      AnimationProvider ,
-      TimeLineCard
-      },
         name: 'TimeLine',
+        props : {
+
+        },
         data() {
             return {
                 list : Array(3).fill("dummy text")
             }
         },
         mounted(){
-            
+            aos.init();
         }
     }
 </script>
-<style lang="scss" >
-.time-line{
-    margin-bottom: 4px;
-    .time-line__row{
-        .time-line__row__card{
-            position: relative;
-            &::before{
-                display: inline-block;
-                content: "";
-                position: absolute;
-                height: 100%;
-                left: 50%;
-                top: 0;
-                transform: translate(-50% , 0);
-                border-left: 1px solid red;
-            }
-        }
-        .time-line__row__title{
-            color: red;
-            display: inline-block;
-            position: relative;
-            left: 50%;
-            transform: translate(-50%, 0);
-        }
-    }
-}
-</style>
