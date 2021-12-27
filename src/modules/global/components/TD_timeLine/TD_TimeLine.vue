c039f7cf47dd1c039e4aa1d2eff7427ebe6ecd84
kamalMohamed <kmohamed@turndigital.net>
2021-12-27 13:47:28 +0200

    progress commit

--- src/modules/global/components/TD_timeLine/TD_TimeLine.vue
+++ src/modules/global/components/TD_timeLine/TD_TimeLine.vue
@@ -1,34 +1,25 @@
 
 <template>
-<section class="td-generic-timeline container-fluid" v-for="( card , index ) in list" :key="index">
-        <div class="td-generic-timeline__row">
-            <h3 class="td-generic-timeline__row__title">2020</h3>
-            <div  class="td-generic-timeline__row__card row"
-            :class="[ index%2  ? 'justify-content-end' : 'justify-content-start']"
-            >
-                <div class="col-md-6 col-12"   >
-                            <slot></slot>
-                </div>
-            </div>
-        </div>
+<section v-for="( card , index ) in cards" :key="index">
+            <slot :card="card" :index="index" ></slot>
 </section>
 </template>
 
 <script>
 import "./TD_timeline.scss"
-import aos from "aos";
     export default {
         name: 'TimeLine',
         props : {
-
+            cards :{
+                type : Array ,
+                required : true ,
+            },
         },
         data() {
             return {
-                list : Array(3).fill("dummy text")
             }
         },
         mounted(){
-            aos.init();
         }
     }
 </script>
