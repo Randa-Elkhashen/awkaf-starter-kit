015d4d96dd0ba2af8309b60005aae618eda00955
kamalMohamed <kmohamed@turndigital.net>
2021-12-27 13:47:42 +0200

    progress commit

--- src/modules/global/views/TD-timeline-Demo.vue
+++ src/modules/global/views/TD-timeline-Demo.vue
@@ -1,12 +1,26 @@
 <template>
-  <timeline >
-      <time-line-card />
+  <timeline  class=" container-fluid"  :cards="list" v-slot="{ card , index }" >
+      <div class="td-generic-timeline">
+            <div class="td-generic-timeline__row">
+              <h3 class="td-generic-timeline__row__title">2020</h3>
+                <div  class="td-generic-timeline__row__card row"
+                :class="[ index%2  ? 'justify-content-end' : 'justify-content-start']"
+                :data-aos="index%2 ? 'fade-left' : 'fade-right'"
+                >
+                    <div class="col-md-6 col-12"   >
+                              <time-line-card :model="card" />  
+                    </div>
+                </div>
+            </div>
+      </div>
   </timeline>
 </template>
 
 <script>
 import Timeline from "../components/TD_timeLine/TD_TimeLine.vue"
 import TimeLineCard from './TD_TimeLineCard.vue';
+import aos from "aos";
+import 'aos/dist/aos.css';
 
 export default {
   name: 'TD-Listing-Demo',
@@ -16,10 +30,14 @@ export default {
   },
       data() {
         return {
+                list : Array(10).fill("dummy text")
         }
     },
     computed : {
 
+    },
+    mounted() {
+            aos.init();
     },
 }
 </script>
