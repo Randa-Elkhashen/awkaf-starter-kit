2723a88a5db969d741bbd3c37bc7a89325279470
kamalMohamed <kmohamed@turndigital.net>
2022-01-25 17:53:20 +0200

    adjust gallery style

--- src/modules/global/views/TD-Gallery-Demo.vue
+++ src/modules/global/views/TD-Gallery-Demo.vue
@@ -1,35 +1,61 @@
 <template>
-<div class="td-generic-slider">
-    <slider key="main-slider"
-      class="td-generic-slider__main-slider"
-    :swiperModules="swiperModules"
-    :swiperParams="swiperParams"
-    :images="images"
-    :bindSwiperWithThumbs="true"
-    :thumbsSwiper="thumbs"
-    >
-      <template v-slot:slide="{ slideModel }">
-                <img :src="slideModel.thumb" />
-      </template>
-    </slider>
-    <thumb-slider   key="thumbs-slider"
-      class="td-generic-slider__thumbs-slider"
-      :swiperModules="thumbsConfig.swiperModules"
-      :swiperParams="thumbsConfig.swiperParams"
+<div class="container">
+  <div class="td-generic-slider">
+      <slider key="main-slider"
+        class="td-generic-slider__main-slider"
+      :swiperModules="swiperModules"
+      :swiperParams="swiperParams"
       :images="images"
-      @thumbsMounted="setMainSliderThumb"
-    >
-      <template v-slot:slide="{ slideModel }" >
-                <img :src="slideModel.thumb" />
-      </template>
-    </thumb-slider>
+      :bindSwiperWithThumbs="true"
+      :thumbsSwiper="thumbs"
+      >
+        <template v-slot:slide="{ slideModel }">
+            <div class="slide-img">
+                  <img  :src="slideModel.thumb" />
+            </div>
+        </template>
+      </slider>
+      <thumb-slider   key="thumbs-slider"
+        class="td-generic-slider__thumbs-slider"
+        :swiperModules="thumbsConfig.swiperModules"
+        :swiperParams="thumbsConfig.swiperParams"
+        :images="images"
+        @thumbsMounted="setMainSliderThumb"
+      >
+        <template v-slot:slide="{ slideModel }" >
+          <div class="slide-img">
+                  <img :src="slideModel.thumb" />
+          </div>
+        </template>
+      </thumb-slider>
+  </div>
 </div>
 
 </template>
 <script src="./TD-galleryDemo.js" ></script>
 <style  lang="scss">
-body {
+.container{
+  padding-top: 20px;
+}
+.td-generic-slider {
   background: #000;
-  color: #000;
+  .slide-img{
+    aspect-ratio: 3;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: lightgray;
+    img {
+      display: block;
+      max-width: 100%;
+      max-height: 100%;
+      object-fit: cover;
+    }
+  }
+  .td-generic-slider__thumbs-slider{
+    .slide-img{
+      aspect-ratio: 2;
+    }
+  }
 }
 </style>
