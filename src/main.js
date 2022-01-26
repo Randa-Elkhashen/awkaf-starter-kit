eb934311afc8fade5dd75f20432decb28783677e
kamalMohamed <kmohamed@turndigital.net>
2022-01-26 11:21:32 +0200

    add directive imports

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import store from './store'
 import setHttpDefaults from './services/http/http-defaults'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import LazyLoadDirective from "./directive/LazyLoadDirective";
  const app = createApp(App)
  app.use(router)
  app.use(store)
+ app.directive('lazyload', LazyLoadDirective)
  app.mount('#app')
  setHttpDefaults()
 
-export default app;
+ export default app;
