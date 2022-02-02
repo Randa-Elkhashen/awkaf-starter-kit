4842372eab85de3944ad5068edd2a72b669df85d
kamalMohamed <kmohamed@turndigital.net>
2022-02-02 13:38:02 +0200

    add register route

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -13,6 +13,11 @@ const routes = [
         path: '/resetPassword',
         name: 'resetPassword',
         component: () => import('./views/ResetPasswordPage.vue') ,
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('./views/RegistrationPage.vue') ,
     }
     
 ]
