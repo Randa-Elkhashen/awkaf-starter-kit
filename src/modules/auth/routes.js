ed551015be0d4581b0534ec140d287bbd2ef4a8a
kamalMohamed <kmohamed@turndigital.net>
2022-02-06 11:40:41 +0200

    modules routes for template

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,24 +1,4 @@
 const routes = [
-    {
-        path: '/login',
-        name: 'Login',
-        component: () => import('./views/LoginPage.vue') ,
-    },
-    {
-        path: '/forgotpassword',
-        name: 'ForgotPassword',
-        component: () => import('./views/ForgotPasswordPage.vue') ,
-    },
-    {
-        path: '/resetPassword',
-        name: 'resetPassword',
-        component: () => import('./views/ResetPasswordPage.vue') ,
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('./views/RegistrationPage.vue') ,
-    }
     
 ]
 
--- src/modules/global/routes.js
+++ src/modules/global/routes.js
@@ -11,52 +11,7 @@ const routes = [
         path: '/',
         name: 'Home',
         component: () => import('../../views/Home.vue')
-    },
-    {
-        path: '/lightbox',
-        name: 'lightbox',
-        component: () => import('./views/TD-lightBox-Demo.vue')
-    },
-    {
-        path: '/lightbox2',
-        name: 'lightbox2',
-        component: () => import('./views/TD-lightBox-Demo2.vue')
-    },
-    {
-        path: '/tdtimeline',
-        name: 'tdtimeline',
-        component: () => import('./views/TD-timeline-Demo.vue')
-    },
-    {
-        path: '/tdtimeline2',
-        name: 'tdtimeline2',
-        component: () => import('./views/TD-timeline-Demo2.vue')
-    },
-    {
-        path: '/tdLisiting1',
-        name: 'tdLisiting1',
-        component: () => import('./views/TD-Listing-Demo.vue')
-    },
-    {
-        path: '/tdLisiting2',
-        name: 'tdLisiting2',
-        component: () => import('./views/TD-Listing-Demo2.vue')
-    },
-    {
-        path: '/listing',
-        name: 'Listing',
-        component: () => import('./views/TD_Listing_View/Listing.vue')
-    },
-    {
-        path: '/tdgallery',
-        name: 'tdgallery',
-        component: () => import('./views/TD-Gallery-Demo.vue')
-    },
-    {
-        path: '/contactUs',
-        name: 'Contact Us',
-        component: () => import('./views/ContactUsDemo.vue')
-    },
+    }
     
 ]
 
