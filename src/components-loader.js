c459dc29003b4389da8a49e6fc7054d348f548e1
Abdelrahman Waziry <awaziry@turndigital.net>
2022-03-21 14:49:31 +0200

    Change main.js usage of components-loader

---
+++ src/components-loader.js
@@ -0,0 +1,7 @@
+// Import component to be loaded like the following
+// import { load as loadExample } from './modules/<ModuleName>/<ComponentName>Loader.js';
+
+export function loadComponents(){
+    // call the loading function here
+    // loadExample()
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 import { setHttpDefaults } from '@/services'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import LazyLoadDirective from "./directive/LazyLoadDirective";
 import initValidationsCustomDefaultMessage from "./modules/global/components/TD_validations/validationsConfig"
 initValidationsCustomDefaultMessage();
- const app = createApp(App)
- app.use(router)
- app.use(store)
- app.directive('lazyload', LazyLoadDirective)
- app.mount('#app')
- setHttpDefaults()
+import { loadComponents } from './components-loader'
 
- export default app;
+setHttpDefaults()
+loadComponents();
